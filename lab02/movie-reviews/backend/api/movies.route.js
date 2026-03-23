5083dd8e245b01cbd80c46a54d7ed83720eba4e1
NamTruong <Sirpotato070605@gmail.com>
2026-03-24 01:36:08 +0700

    lap2-2

--- lab02/movie-reviews/backend/api/movies.route.js
+++ lab02/movie-reviews/backend/api/movies.route.js
@@ -1,8 +1,10 @@
 import express from 'express';
+import MoviesController from './movies.controller.js';
+
 const router = express.Router();
 
-router.route('/').get((req, res) => 
-    res.send("Hello, Movies API"));
+router.route('/').get(MoviesController.apiGetMovies);
+
 export default router;
 
 
